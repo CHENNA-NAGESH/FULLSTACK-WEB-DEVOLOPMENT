670d3a7dc58be4f9d10be80a0c5259503378cc05
CHENNA-NAGESH <nageshch9966@gmail.com>
2024-02-13 11:50:49 +0530

    submitted

---
+++ 4.Javascript/JAVASCRIPT TASKS/1.Users Task/code.js
@@ -0,0 +1,30 @@
+var users = [
+    {
+        Name : 'Ali',
+        Pin_number : '21255-CM-001',
+        Village : 'Rajahmundry',
+        Phone_Number : 1111111111,
+        Gender : 'Male'
+    },
+    {
+        Name : 'Nagesh',
+        Pin_number : '21255-CM-006',
+        Village : 'Rajahmundry',
+        Phone_Number : 2222222222,
+        Gender : 'Male'
+    },
+    {
+        Name : 'Dileep',
+        Pin_number : '21255-CM-054',
+        Village : 'Kakinada',
+        Phone_Number : 3333333333,
+        Gender : 'Male'
+    },
+    {
+        Name : 'Pradeep',
+        Pin_number : '21255-CM-024',
+        Village : 'Ravulapalem',
+        Phone_Number : 4444444444,
+        Gender : 'Male'
+    }
+];
