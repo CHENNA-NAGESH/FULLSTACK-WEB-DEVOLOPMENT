d2c361c771d14c9b34bc631265f2938dfba827b8
CHENNA-NAGESH <nageshch9966@gmail.com>
2024-02-13 09:44:02 +0530

    submitted

---
+++ 4.Javascript/EXTERNAL JAVASCRIPT FILES/external javascript.js
@@ -0,0 +1,4 @@
+function myFunction()
+{
+    alert("Welcome to Javascript!!");
+}
---
+++ 4.Javascript/EXTERNAL JAVASCRIPT FILES/variable.js
@@ -0,0 +1,5 @@
+var name="Nagesh";
+var name="Dileep";
+let roll_number='212255-cm-006';
+roll_number='21255-cm-054';
+const college = 'Aditya';
